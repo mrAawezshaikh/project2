dfe30610edbf50e52c17e630f8c83848df08c2b5
mrAawezshaikh <mr.shaikh.developer@gmail.com>
2023-10-06 16:52:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div className='main'>
+    <h1>School Days</h1>
+    <div className='sub'>
+      <h5>School Freiend</h5>
+      <ol>
+        <li>Rahul</li>
+        <li>Sumit</li>
+        <li>Feroz</li>
+        <li>Sameer</li>
+      </ol>
+      </div>
+      <div className='sub'>
+        <h5>School Routing</h5>
+        <ol type='a'>
+          <li>I used To Woke Up Early</li>
+          <li>I used To Brush My Teeth</li>
+          <li>I used To take bath</li>
+          <li>I used To catch my school bus          </li>
+        </ol>
+      
+   </div>
+   <div className='sub'>
+    <h5>School Games</h5>
+    <ol>
+      <li>Crricket</li>
+      <li>Football</li>
+      <li>Kabaddi</li>
+      <li>Karate</li>
+    </ol>
+
+   </div>
+   <div className='sub'>
+    <h5>School Subject</h5>
+    <ol>
+      <li>Maths</li>
+      <li>Marathi</li>
+      <li>english</li>
+      <li>Urdu</li>
+
+    </ol>
+   </div>
+   </div>
+   
+  );
+}
+
+export default App;
